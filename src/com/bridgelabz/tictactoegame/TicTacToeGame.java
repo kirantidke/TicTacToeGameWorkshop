ad55955292182e9b974d2582db41a059332f79bf
kirantidke <kirantidke2013@gmail.com>
2021-07-01 11:08:21 +0530

    "[kiran] added code for creating board"

--- src/com/bridgelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoegame/TicTacToeGame.java
@@ -1,9 +1,22 @@
 package com.bridgelabz.tictactoegame;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+	private static final char empty = ' ';
+	private char[] board;
+
+	TicTacToeGame() {
+		board = new char[10];
+		for (int i = 0; i < board.length; i++) {
+			board[i] = empty;
+		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println("welcome to TicTacToe game");
-		}
 
+		TicTacToeGame game = new TicTacToeGame();
+		 game.TicTacToeGame();
+	}
 }
