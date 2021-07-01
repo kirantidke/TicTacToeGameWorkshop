5c1272f3192598effd29c93bbd085977dcf5f5bc
kirantidke <kirantidke2013@gmail.com>
2021-07-01 11:18:28 +0530

    "[kiran] added code to show board"

--- src/com/bridgelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoegame/TicTacToeGame.java
@@ -24,10 +24,10 @@ public class TicTacToeGame {
 	public void choosePlayerSymbol(char playerSymbol) {
 		if (playerSymbol == cross) {
 			this.playerSymbol = cross;
-			this.computerSymbol = round;
+			this.computerSymbol = cross;
 		} else if (playerSymbol == round) {
 			this.playerSymbol = round;
-			this.computerSymbol = round;
+			this.computerSymbol = cross;
 		} else
 			System.out.println("Invalid Symbol");
 	}
@@ -36,14 +36,31 @@ public class TicTacToeGame {
 		return playerSymbol;
 	}
 
+	public void showBoard() {
+		for (int i = 1; i <= 3; i++) {
+			for (int j = 1; j <= 3; j++) {
+				if (j == 3)
+					System.out.print(board[getIndex(i, j)]);
+				else
+					System.out.print(board[getIndex(i, j)] + " | ");
+
+			}
+			System.out.println("");
+			if (i != 3)
+				System.out.println("-------------------");
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to TicTacToe game ");
 		TicTacToeGame game = new TicTacToeGame();
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Choose a symbol X or O: ");
+		System.out.println("Choose a symbol (X or O): ");
 		String symbol = sc.next();
 		game.choosePlayerSymbol(symbol.charAt(0));
 		System.out.println("Player has chosen: " + game.getPlayerSymbol());
 
+		System.out.println("Game Board is: ");
+
+		game.showBoard();
 	}
 }
