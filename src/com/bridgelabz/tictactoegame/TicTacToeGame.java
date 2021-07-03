7f016b6f229294f8d79fb64482446f36ab2ae800
kirantidke <kirantidke2013@gmail.com>
2021-07-03 16:51:55 +0530

    "[kiran] added code to move player"

--- src/com/bridgelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoegame/TicTacToeGame.java
@@ -1,20 +1,21 @@
 package com.bridgelabz.tictactoegame;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	private static final char empty = ' ';
-	private static final char cross = 'X';
-	private static final char round = 'O';
-	private static char playerSymbol;
-	private static char computerSymbol;
+	private static final char EMPTY = ' ';
+	private static final char CROSS = 'X';
+	private static final char ROUND = 'O';
 
+	private char playerSymbol;
+	private char computerSymbol;
 	private char[] board;
 
 	TicTacToeGame() {
 		board = new char[10];
-		for (int i = 0; i < board.length; i++)
-			this.board[i] = empty;
+		Arrays.fill(board, EMPTY);
+
 	}
 
 	private static int getIndex(int row, int col) {
@@ -22,18 +23,64 @@ public class TicTacToeGame {
 	}
 
 	public void choosePlayerSymbol(char playerSymbol) {
-		if (playerSymbol == cross) {
-			this.playerSymbol = cross;
-			this.computerSymbol = cross;
-		} else if (playerSymbol == round) {
-			this.playerSymbol = round;
-			this.computerSymbol = cross;
+		if (playerSymbol == CROSS) {
+			this.playerSymbol = CROSS;
+			this.computerSymbol = ROUND;
+		} else if (playerSymbol == ROUND) {
+			this.playerSymbol = ROUND;
+			this.computerSymbol = CROSS;
 		} else
 			System.out.println("Invalid Symbol");
 	}
 
 	public char getPlayerSymbol() {
 		return playerSymbol;
+	}
+
+	private boolean isFree(int row, int col) {
+		if (row > 3 || row < 1 || col > 3 || col < 1)
+			System.out.println("Invalid position!");
+
+		if (board[getIndex(row, col)] == EMPTY)
+			return true;
+		return false;
+	}
+
+	private boolean isFree(int position) {
+		if (position < 1 || position > 9)
+			System.out.println("Invalid position!");
+		if (board[position - 1] == EMPTY)
+			return true;
+		return false;
+	}
+
+	public void playerMove(int row, int col) {
+		if (row > 3 || row < 1 || col > 3 || col < 1) {
+			System.out.println("Invalid move!");
+			return;
+		}
+
+		if (isFree(row, col)) {
+			board[getIndex(row, col)] = playerSymbol;
+			System.out.println("After player move");
+			showBoard();
+		} else
+			System.out.println("Illegal move!");
+
+	}
+
+	public void playerMove(int position) {
+		if (position < 1 || position > 9) {
+			System.out.println("Invalid move!");
+			return;
+		}
+
+		if (isFree(position)) {
+			board[position - 1] = playerSymbol;
+			System.out.println("After player move");
+			showBoard();
+		} else
+			System.out.println("Illegal move!");
 	}
 
 	public void showBoard() {
@@ -47,7 +94,7 @@ public class TicTacToeGame {
 			}
 			System.out.println("");
 			if (i != 3)
-				System.out.println("-------------------");
+				System.out.println("---------");
 		}
 	}
 
@@ -58,9 +105,16 @@ public class TicTacToeGame {
 		String symbol = sc.next();
 		game.choosePlayerSymbol(symbol.charAt(0));
 		System.out.println("Player has chosen: " + game.getPlayerSymbol());
-
-		System.out.println("Game Board is: ");
-
+		System.out.println("initial:");
 		game.showBoard();
+		System.out.println("Enter position to move: ");
+		int position = sc.nextInt();
+
+		System.out.println(game.isFree(3));
+		// game.isFree(3);
+
+		game.playerMove(3);
+		System.out.println("player moved by " + position);
+
 	}
 }
